5198488d414d1de43d9b6f25ab5d8eb639557eb8
raaaaaiin <marcraineer0089@gmail.com>
2021-09-24 19:43:44 +0800

    Adjusted Log in Design

--- Surveillance_FaceRecognition/SSlogIn.Designer.cs
+++ Surveillance_FaceRecognition/SSlogIn.Designer.cs
@@ -30,13 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SSlogIn));
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.Button1 = new Bunifu.Framework.UI.BunifuFlatButton();
             this.Textbox2 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.Textbox1 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,36 +44,28 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Roboto", 8F);
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(25, 281);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.label1.Location = new System.Drawing.Point(25, 260);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Surveillance_FaceRecognition.Properties.Resources.Asset_4;
-            this.pictureBox1.Location = new System.Drawing.Point(22, 22);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(241, 108);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(187, 275);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(86)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(187, 254);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Configure";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(142, 247);
+            this.panel1.Location = new System.Drawing.Point(149, 226);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(121, 89);
             this.panel1.TabIndex = 7;
@@ -81,8 +73,8 @@
             // Button1
             // 
             this.Button1.Active = false;
-            this.Button1.Activecolor = System.Drawing.Color.Gold;
-            this.Button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Button1.Activecolor = System.Drawing.Color.Goldenrod;
+            this.Button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(86)))));
             this.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Button1.BorderRadius = 0;
             this.Button1.ButtonText = "LogIn";
@@ -100,9 +92,9 @@
             this.Button1.IconVisible = true;
             this.Button1.IconZoom = 90D;
             this.Button1.IsTab = false;
-            this.Button1.Location = new System.Drawing.Point(22, 299);
+            this.Button1.Location = new System.Drawing.Point(22, 278);
             this.Button1.Name = "Button1";
-            this.Button1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Button1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(86)))));
             this.Button1.OnHovercolor = System.Drawing.Color.Gold;
             this.Button1.OnHoverTextColor = System.Drawing.Color.White;
             this.Button1.selected = false;
@@ -121,16 +113,16 @@
             this.Textbox2.characterCasing = System.Windows.Forms.CharacterCasing.Normal;
             this.Textbox2.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.Textbox2.Font = new System.Drawing.Font("Roboto", 12F);
-            this.Textbox2.ForeColor = System.Drawing.Color.White;
+            this.Textbox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
             this.Textbox2.HintForeColor = System.Drawing.Color.White;
             this.Textbox2.HintText = "Password";
             this.Textbox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.Textbox2.isPassword = false;
-            this.Textbox2.LineFocusedColor = System.Drawing.Color.Gold;
-            this.Textbox2.LineIdleColor = System.Drawing.Color.DimGray;
-            this.Textbox2.LineMouseHoverColor = System.Drawing.Color.Gold;
+            this.Textbox2.isPassword = true;
+            this.Textbox2.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Textbox2.LineIdleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(86)))));
+            this.Textbox2.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.Textbox2.LineThickness = 4;
-            this.Textbox2.Location = new System.Drawing.Point(22, 199);
+            this.Textbox2.Location = new System.Drawing.Point(22, 183);
             this.Textbox2.Margin = new System.Windows.Forms.Padding(4);
             this.Textbox2.MaxLength = 32767;
             this.Textbox2.Name = "Textbox2";
@@ -145,15 +137,15 @@
             this.Textbox1.characterCasing = System.Windows.Forms.CharacterCasing.Normal;
             this.Textbox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.Textbox1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Textbox1.ForeColor = System.Drawing.Color.White;
-            this.Textbox1.HintForeColor = System.Drawing.Color.White;
+            this.Textbox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.Textbox1.HintForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
             this.Textbox1.HintText = "Username";
             this.Textbox1.isPassword = false;
-            this.Textbox1.LineFocusedColor = System.Drawing.Color.Gold;
-            this.Textbox1.LineIdleColor = System.Drawing.Color.DimGray;
-            this.Textbox1.LineMouseHoverColor = System.Drawing.Color.Gold;
+            this.Textbox1.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Textbox1.LineIdleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(131)))), ((int)(((byte)(86)))));
+            this.Textbox1.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.Textbox1.LineThickness = 4;
-            this.Textbox1.Location = new System.Drawing.Point(22, 158);
+            this.Textbox1.Location = new System.Drawing.Point(22, 139);
             this.Textbox1.Margin = new System.Windows.Forms.Padding(4);
             this.Textbox1.MaxLength = 32767;
             this.Textbox1.Name = "Textbox1";
@@ -164,20 +156,30 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(22, 247);
+            this.button3.Location = new System.Drawing.Point(22, 226);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
+            this.button3.Text = "QR Code";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // SSlogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(282, 359);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(282, 350);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.Button1);
             this.Controls.Add(this.panel1);
@@ -202,10 +204,10 @@
         private Bunifu.Framework.UI.BunifuFlatButton Button1;
         private Bunifu.Framework.UI.BunifuMaterialTextbox Textbox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
